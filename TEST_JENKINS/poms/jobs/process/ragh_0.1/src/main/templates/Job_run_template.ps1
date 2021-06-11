d2dca65969c4a1a5409866087944d2334bddcf51
Smitha SJ <smithasj.talend.com@talendconsultingap.aws>
2021-06-11 20:16:38 +0530

    User 'smithasj.talend.com@talendconsultingap.aws' at 20:16:36 IST 2021-06-11 : Update project 'test_jenkins'   Save project 'test_jenkins'

---
+++ TEST_JENKINS/poms/jobs/process/ragh_0.1/src/main/templates/Job_run_template.ps1
@@ -0,0 +1,3 @@
+$fileDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+cd $fileDir
+java '-Dtalend.component.manager.m2.repository=%cd%/../lib' '-Xms256M' '-Xmx1024M' '-Dfile.encoding=UTF-8' -cp '.;../lib/routines.jar;../lib/log4j-to-slf4j-2.12.1.jar;../lib/slf4j-log4j12-1.7.25.jar;../lib/log4j-1.2.17.jar;../lib/job-audit-1.4.jar;../lib/commons-lang3-3.8.1.jar;../lib/audit-log4j1-1.16.1.jar;../lib/json-smart-2.2.1.jar;../lib/accessors-smart-1.1.jar;../lib/logging-event-layout-1.16.1.jar;../lib/slf4j-api-1.7.25.jar;../lib/asm-5.0.3.jar;../lib/audit-common-1.16.1.jar;ragh_0_1.jar;' test_jenkins.ragh_0_1.ragh --context=Default $args
